aca9455a4ee44f5c4980d4d87294b1b8dac75b09
Yoshiyuki Hirano <yhirano@me.com>
2017-10-09 02:54:02 +0900

    Add DEFAULT_FORMATTER constant to Config

--- lib/table_help/config.rb
+++ lib/table_help/config.rb
@@ -1,16 +1,39 @@
 module TableHelp
   class Config
-    attr_accessor :default_options
+    DEFAULT_OPTIONS = {
+      table_for: {
+        class: "table table-striped table-hover table_for",
+      },
+      attributes_table_for: {
+        class: "table table-striped table-hover attributes_table_for",
+      },
+    }.freeze
+
+    DEFAULT_FORMATTER = {
+      attribute_name: ->(name, collection_or_resource) do
+        if collection_or_resource.respond_to?(:model)
+          collection_or_resource.model.human_attribute_name(name)
+        else
+          collection_or_resource.class.human_attribute_name(name)
+        end
+      end,
+      value: ->(name, value) do
+        case value
+        when DateTime, Time
+          I18n.l(value)
+        when Numeric
+          (name.to_sym == :id) ? value : value.to_s(:delimited)
+        else
+          value
+        end
+      end,
+    }.freeze
+
+    attr_accessor :default_options, :formatter
 
     def initialize
-      @default_options = {
-        table_for: {
-          class: "table table-striped table-hover table_for",
-        },
-        attributes_table_for: {
-          class: "table table-striped table-hover attributes_table_for",
-        },
-      }
+      @default_options = DEFAULT_OPTIONS
+      @formatter = DEFAULT_FORMATTER
     end
   end
 end
